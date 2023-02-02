cc259b702c984d348547d60ea94851bb066fc5a5
Rufet Huseynov <103166770+ruhuseyn@users.noreply.github.com>
2023-02-02 23:28:55 +0400

    Changes

--- src/main/java/com/example/demoemployeecrud/conf/SecurityConfig.java
+++ src/main/java/com/example/demoemployeecrud/conf/SecurityConfig.java
@@ -3,8 +3,12 @@ package com.example.demoemployeecrud.conf;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,50 +17,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true,
-        jsr250Enabled = true)
-@Slf4j
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
+
     @Bean
     public BCryptPasswordEncoder getBCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return null;
-            }
-        };
-    }
-//        return username -> {
-//            return accountDataRepo.findByUsername(username).map(account -> User.builder()
-//                    .username(account.getUsername())
-//                    .password(account.getPassword())
-//                    .roles("ADMIN")
-//                    .build()).orElseThrow(() -> new UsernameNotFoundException("Not found"));
-        };
 
-//    @Bean
-//    public UserDetailsService users(BCryptPasswordEncoder bCryptPasswordEncoder) {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password(bCryptPasswordEncoder.encode("123456"))
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password(bCryptPasswordEncoder.encode("123456"))
-//                .roles("USER", "ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication()
+                .withUser("ruhuseyn")
+                .password(getBCryptPasswordEncoder().encode("12345"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(getBCryptPasswordEncoder().encode("12345"))
+                .roles("USER","ADMIN");
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/api/v1/employees/free");
     }
 }
